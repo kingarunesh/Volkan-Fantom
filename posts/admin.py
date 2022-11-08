2d50dcb740ee7fb34cf868d9eafcaf50e7bbb9c5
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-11-08 22:15:26 +0530

    Index page displaying

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from posts.models import Post
 
-# Register your models here.
+
+class AdminPost(admin.ModelAdmin):
+    list_filter = ["publishing_date"]
+    list_display = ["title", "publishing_date"]
+    search_fields = ["title", "content"]
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, AdminPost)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from posts.models import Post
 
-# Create your views here.
+
+class IndexView(ListView):
+    model = Post
+    template_name = "posts/index.html"
+    context_object_name = "posts"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        return context
