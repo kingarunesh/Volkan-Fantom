2a16c4f619b14ceffbf96c63176c128085ebe0ed
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-11-09 01:58:11 +0530

    2.1 : Category -> Context (templatetags), post_count method in category model and sidebar.html modifed

--- posts/models.py
+++ posts/models.py
@@ -13,6 +13,9 @@ class Category(models.Model):
         
     def __str__(self):
         return self.title
+    
+    def post_count(self):
+        return self.posts.all().count()
 
 
 class Post(models.Model):
@@ -22,7 +25,7 @@ class Post(models.Model):
     image = models.ImageField(blank=True, null=True, upload_to="uploads/")
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     slug = models.SlugField(default="slug", editable=False)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1, related_name="posts")
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
