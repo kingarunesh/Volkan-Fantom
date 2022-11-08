d67ff1393eec7a59f55da9471060878f3ce17974
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-11-09 00:34:22 +0530

    1.3 : Category models added

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from posts.models import Post
+from posts.models import Post, Category
 
 
 class AdminPost(admin.ModelAdmin):
@@ -12,3 +12,4 @@ class AdminPost(admin.ModelAdmin):
 
 
 admin.site.register(Post, AdminPost)
+admin.site.register(Category)
--- posts/models.py
+++ posts/models.py
@@ -3,15 +3,30 @@ from django.conf import settings
 from django.template.defaultfilters import slugify
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(editable=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+        
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     publishing_date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(blank=True, null=True, upload_to="uploads/")
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    slug = models.SlugField(default="slug")
+    slug = models.SlugField(default="slug", editable=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(Post, self).save(*args, **kwargs)
         
+    def __str__(self):
+        return self.title
