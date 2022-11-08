ad8c4bc9b077e9f1ab90f1ea37a7c83d4d34164f
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-11-09 02:55:09 +0530

    2.2 : Category -> category posts modifed (views, urls, html)

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,6 @@ from posts.views import *
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path("detail/<int:pk>/<slug:slug>", PostDetail.as_view(), name="detail")
+    path("detail/<int:pk>/<slug:slug>", PostDetail.as_view(), name="detail"),
+    path("category/<int:pk>/<slug:slug>", CategoryDetail.as_view(), name="category_detail")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
-from posts.models import Post
+from posts.models import Post, Category
+from django.shortcuts import get_object_or_404
 
 
 class IndexView(ListView):
@@ -19,4 +20,20 @@ class PostDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(PostDetail, self).get_context_data(**kwargs)
+        return context
+
+
+class CategoryDetail(ListView):
+    model = Post
+    template_name = "categories/category-detail.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, pk=self.kwargs["pk"])
+        return Post.objects.filter(category=self.category).order_by("-id")
+
+    def get_context_data(self,**kwargs):
+        context = super(CategoryDetail, self).get_context_data(**kwargs)
+        self.category = get_object_or_404(Category, pk=self.kwargs["pk"])
+        context["category"] = self.category
         return context
