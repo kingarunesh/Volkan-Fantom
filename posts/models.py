ba7913192ced14045bf0af02f4611cfc796d6b73
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-11-09 10:22:17 +0530

    3.1 : Tag model

--- posts/models.py
+++ posts/models.py
@@ -18,6 +18,18 @@ class Category(models.Model):
         return self.posts.all().count()
 
 
+class Tag(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(editable=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Tag, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -26,6 +38,7 @@ class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     slug = models.SlugField(default="slug", editable=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1, related_name="posts")
+    tag = models.ManyToManyField(Tag, related_name="posts", blank=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
