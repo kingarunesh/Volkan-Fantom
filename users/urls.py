2935d2922f894a12d68a9d580fc5993b12ec9b02
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-11-11 20:12:45 +0530

    10.6 : password change -> ( urls.py | password-change_form.html )

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from users.views import *
+from django.contrib.auth import views as authView
 
 
 app_name = "users"
@@ -7,5 +8,7 @@ app_name = "users"
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
-    path("logout/", UserLogoutView.as_view(), name="logout")
+    path("logout/", UserLogoutView.as_view(), name="logout"),
+    path("password-change/", authView.PasswordChangeView.as_view(), name="password_change"),
+    path("password-change-done/", authView.PasswordChangeDoneView.as_view(), name="password_change_done"),
 ]
